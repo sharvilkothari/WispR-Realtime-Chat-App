cdfaf069c19c676262b25ce15acc7b4ac47907d1
sharvilkothari <sharvilkothari10@gmail.com>
2025-04-14 04:26:42 +0530

    Project Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,12 +18,11 @@ app.use(cors({
 }));
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes);
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
-  app.get("*", (req, res) => {
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
 
 const startServer = async () => {
     try {
