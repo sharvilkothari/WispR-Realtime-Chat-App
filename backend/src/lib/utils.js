e8ac06976bf50020602e82623800a46dbfdba196
sharvilkothari <sharvilkothari10@gmail.com>
2025-04-14 04:29:53 +0530

    Project Done

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken=(userId, res)=>{
         maxAge:7*24*60*60*1000,
         httpOnly:true,
         sameSite:"strict",
-        secure:process.env.NODE_ENV!=="development",
+        secure:true,
     });
     return token;
 }
